d3930e1d03c00ccf31e8aa832e02928af14098de
duogbachdev <contact.duogbachdev@gmail.com>
2023-08-27 00:39:02 +0700

    Update

--- MyProject3/src/page/admin/LayoutAdmin.tsx
+++ MyProject3/src/page/admin/LayoutAdmin.tsx
@@ -4,22 +4,27 @@ import { Link } from "react-router-dom";
 
 const LayoutAdmin = () => {
   return (
-    <div>
-      <div className="flex gap-[300px]">
+    <div className="flex gap-[300px]">
+      <div className="bg-gray-600  px-8 py-8 h-screen w-1/6">
+        <h1 className=" uppercase text-2xl mb-5">DuogBachDev</h1>
         <div>
-          <div>
-            <Link to="addproduct">add product</Link>
-          </div>
+          <Link to="">Dashboard</Link>
+        </div>
+        <div>
+          <Link to="product"> product</Link>
+        </div>
+        <div>
+          <Link to="addproduct">add product</Link>
+        </div>
 
-          <div>
-            <Link to="product/:id/edit">edit product</Link>
-          </div>
-        </div>
         <div>
-          <Outlet />
+          <Link to="product/:id/edit">edit product</Link>
         </div>
-        {/* hien thi thanh phan con ben trong router  */}
       </div>
+      <div className="w-5/6">
+        <Outlet />
+      </div>
+      {/* hien thi thanh phan con ben trong router  */}
     </div>
   );
 };
